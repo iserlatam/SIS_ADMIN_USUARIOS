eecb9107287d0c1bbecb000748240f88dc0bc9b7
iserlatam <iserlatam@gmail.com>
2023-06-08 16:05:59 -0500

    Revert "Backend inicial con las rutas de certificaciones, usuarios y autenticacion inicial"
    This reverts commit 026e9732bbae79e68f9ca1dc94ba71b49561f48c.

--- backend/src/api/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-import * as authCtrl from '../controllers/auth.controllers.js';
-const router = Router();
-
-router.route('/registro').post(authCtrl.registro);
-
-router.route('/ingreso').post(authCtrl.ingreso);
-
-export default router;
